3d8c667beaaf7d2891648e7d78223d5da915f01b
barry <barry.p.evans@gmail.com>
2021-06-12 19:32:34 -0700

    ddol

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -78,7 +78,7 @@ public class Player : MonoBehaviour
     }
     private void Attack()
     {
-        print(gameObject.name + " attacks!");
+        //print(gameObject.name + " attacks!");
         if (isEvil)
         {
             Collider[] hits = Physics.OverlapBox(rb.position + transform.forward, Vector3.one * 0.5f, transform.rotation, playerMask);
@@ -135,6 +135,7 @@ public class Player : MonoBehaviour
         m_gunAttach = transform.Find("gun_attach");
         var gunPrefab = Resources.Load<GameObject>("gun");
         var gunObject = Instantiate(gunPrefab, m_gunAttach.position, transform.rotation);
+        GameObject.DontDestroyOnLoad(gunObject);
         m_gun = gunObject.GetComponent<Gun>();
         m_gun.SetAttachPoint(m_gunAttach);
     }
