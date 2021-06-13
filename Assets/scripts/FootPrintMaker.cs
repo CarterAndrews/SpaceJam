b6ff1e7f0d0a61a1793ad273e0bb7d1f6b9bfcb1
Kain Shepard <kainkun100@gmail.com>
2021-06-12 17:21:09 -0700

    aaaaaaaa

--- Assets/scripts/FootPrintMaker.cs
+++ Assets/scripts/FootPrintMaker.cs
@@ -7,7 +7,7 @@ public class FootPrintMaker : MonoBehaviour
     public GameObject footPrint;
     Vector3 lastStepPosition;
     float strideLength = 1f;
-    float strideWidth = 0.5f;
+    float strideWidth = 0.25f;
     bool isRightFoot;
 
     private void Start()
